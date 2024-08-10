5520960ca0920300cb0f9571f75a3b84af621b29
Daliya <daliyadeutsch@gmail.com>
2024-08-11 02:43:21 +0530

    Current time and song duration.

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
@@ -7,7 +7,6 @@ const PlayerContextProvider = (props) => {
   const audioRef = useRef();
   const seekBg = useRef();
   const seekBar = useRef();
-
   const [track, setTrack] = useState(songsData[5]);
   const [playStatus, setPlayStatus] = useState(false);
   const [time, setTime] = useState({
@@ -31,6 +30,23 @@ const PlayerContextProvider = (props) => {
     setPlayStatus(false);
   };
 
+  useEffect(() => {
+    setTimeout(() => {
+      audioRef.current.ontimeupdate = () => {
+        setTime({
+          currentTime: {
+            second: Math.floor(audioRef.current.currentTime % 60),
+            minute: Math.floor(audioRef.current.currentTime / 60),
+          },
+          totalTime: {
+            second: Math.floor(audioRef.current.duration % 60),
+            minute: Math.floor(audioRef.current.duration / 60),
+          },
+        });
+      };
+    }, 1000);
+  }, [audioRef]);
+
   const contextvalue = {
     audioRef,
     seekBar,
@@ -44,6 +60,7 @@ const PlayerContextProvider = (props) => {
     play,
     pause,
   };
+
   return (
     <PlayerContext.Provider value={contextvalue}>
       {props.children}
