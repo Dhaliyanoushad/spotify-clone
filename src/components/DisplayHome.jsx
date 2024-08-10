2332e02c9a53fa8ede926bd869c8ca97d22a2582
Daliya <daliyadeutsch@gmail.com>
2024-08-10 19:36:06 +0530

    Song Item Component

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -20,6 +20,20 @@ const DisplayHome = () => {
           ))}
         </div>
       </div>
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's biggest hits</h1>
+        <div className="flex overflow-hidden hover:overflow-auto">
+          {songsData.map((item, index) => (
+            <AlbumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
     </>
   );
 };
