ff548c9a65c3d424f912d3fc0be8b42022ee20d3
Daliya <daliyadeutsch@gmail.com>
2024-08-11 13:19:26 +0530

    hover effects

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,11 +34,13 @@ const Navbar = () => {
         </div>
       </div>
       <div className="flex items-center gap-2 mt-4">
-        <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer">
+        <p className="bg-white text-black px-4 py-1 rounded-2xl cursor-pointer hover:bg-black hover:text-white">
           All
         </p>
-        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
-        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer hover:bg-white hover:text-black">
+          Music
+        </p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer hover:bg-white hover:text-black">
           Podcasts
         </p>
       </div>
